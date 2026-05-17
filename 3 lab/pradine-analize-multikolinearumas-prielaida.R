ab6a1f13a7500fb32eea3d0570639cca983d6940
Friebay <frybaylt@gmail.com>
2026-05-17 10:19:16 +0300

    pakeisti tiesiškumo grafikus į ggplot

--- 3 lab/pradine-analize-multikolinearumas-prielaida.R
+++ 3 lab/pradine-analize-multikolinearumas-prielaida.R
@@ -7,6 +7,9 @@ library(StepReg)
 library(RcmdrPlugin.survival)
 library(MASS)
 library(knitr)
+library(ggplot2)
+library(patchwork)
+
 # duomenys iš https://www.kaggle.com/datasets/davinwijaya/employee-turnover?resource=download
 df <- read.csv('turnover.csv', header = TRUE)
 
@@ -192,34 +195,80 @@ summary(step_model)
 
 # Tiesiškumo tikrinimas
 
-kiekybiniai_kintamieji <- c("age", "extraversion", "independ", "selfcontrol", "anxiety", "novator")
-
+kiekybiniai_kintamieji <- c("age", "extraversion", "independ", "selfcontrol", "anxiety")
 res_martingale <- residuals(model_tv1, type = "martingale")
 X <- as.matrix(df_train_split[, kiekybiniai_kintamieji])
 b <- coef(model_tv1)[kiekybiniai_kintamieji]
 
+vertimai <- c(
+  "age"          = "Amžius",
+  "extraversion" = "Ekstraversiškumas",
+  "independ"     = "Savarankiškumas",
+  "selfcontrol"  = "Sąžiningumas",
+  "anxiety"      = "Nerimastingumas",
+  "novator"      = "Novatoriškumas"
+)
+
+res_martingale <- residuals(model_tv1, type = "martingale")
+X <- as.matrix(df_train_split[, kiekybiniai_kintamieji])
+
+lt_format <- function(x) format(x, big.mark = ".", decimal.mark = ",", scientific = FALSE)
+
+plots <- lapply(seq_along(kiekybiniai_kintamieji), function(j) {
+  df_plot <- data.frame(x = X[, j], res = res_martingale)
+  
+  ggplot(df_plot, aes(x = x, y = res)) +
+    geom_point(size = 0.6, alpha = 0.5) +
+    geom_hline(yintercept = 0, linetype = "dashed") +
+    geom_smooth(method = "loess", span = 0.75, se = FALSE,
+                color = "red", linewidth = 0.7) +
+    labs(x = vertimai[kiekybiniai_kintamieji[j]], y = "Martingalo liekanos") +
+    scale_x_continuous(labels = lt_format) +
+    scale_y_continuous(labels = lt_format) +
+    theme_bw(base_size = 9) +
+    theme(
+      panel.grid.minor = element_blank(),
+      axis.title = element_text(size = 8),
+      axis.text  = element_text(size = 7)
+    )
+})
+
+combined <- wrap_plots(plots, ncol = 3)
+
+ggsave("martingale-residuals.png", plot = combined,
+       width = 7, height = 4.5, dpi = 750, units = "in")
+
 par(mfrow = c(2, 3))
 
-# Liekanos ir kovariantės
-for (j in seq_along(kiekybiniai_kintamieji)) {
-  plot(X[, j], res_martingale,
-       xlab = kiekybiniai_kintamieji[j],
-       ylab = "Residuals")
-  abline(h = 0, lty = 2)
-  lines(lowess(X[, j], res_martingale, iter = 0), col = "red")
-}
-
-par(mfrow = c(2, 3))
-
-# Kompnentė + liekana ir kovariantės
-for (j in seq_along(kiekybiniai_kintamieji)) {
+plots_cr <- lapply(seq_along(kiekybiniai_kintamieji), function(j) {
   component_resid <- b[j] * X[, j] + res_martingale
-  plot(X[, j], component_resid,
-       xlab = kiekybiniai_kintamieji[j],
-       ylab = "Component + residual")
-  abline(lm(component_resid ~ X[, j]), lty = 2)
-  lines(lowess(X[, j], component_resid, iter = 0), col = "red")
-}
+  df_plot <- data.frame(x = X[, j], cr = component_resid)
+  df_plot <- na.omit(df_plot)
+  
+  lm_fit <- lm(cr ~ x, data = df_plot)
+  df_plot$lm_pred <- fitted(lm_fit)
+  
+  ggplot(df_plot, aes(x = x, y = cr)) +
+    geom_point(size = 0.6, alpha = 0.5) +
+    geom_line(aes(y = lm_pred), linetype = "dashed", linewidth = 0.6) +
+    geom_smooth(method = "loess", span = 0.75, se = FALSE,
+                color = "red", linewidth = 0.7) +
+    labs(x = vertimai[kiekybiniai_kintamieji[j]],
+         y = "Komponentė + liekana") +
+    scale_x_continuous(labels = lt_format) +
+    scale_y_continuous(labels = lt_format) +
+    theme_bw(base_size = 9) +
+    theme(
+      panel.grid.minor = element_blank(),
+      axis.title = element_text(size = 8),
+      axis.text  = element_text(size = 7)
+    )
+})
+
+combined_cr <- wrap_plots(plots_cr, ncol = 3)
+
+ggsave("component-residuals.png", plot = combined_cr,
+       width = 7, height = 4.5, dpi = 750, units = "in")
 
 par(mfrow = c(1, 1))
 
